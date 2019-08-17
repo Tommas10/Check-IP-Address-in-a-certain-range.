bff9356da5d3b59229428b2bc5149974ffd6928b
Tommas10 <46047283+Tommas10@users.noreply.github.com>
2019-08-17 14:38:48 +0800

    Add files via upload

---
+++ CheckIPAddressInRangeV1.ps1
@@ -0,0 +1,21 @@
+﻿#This is small automation PowerShell script to check IP address from the computer and then check the address to see if it is in a certain range.
+#Under Microsoft Windows 10.
+#Created date: 2019-08-17
+#Created by Tommas Huang
+
+
+$ipLow = [IPAddress] "192.168.0.170"
+$ipHigh = [IPAddress] "192.168.0.180"
+
+[Net.NetworkInformation.NetworkInterface]::GetAllNetworkInterfaces() | ? {$_.OperationalStatus -eq "Up"} | % {
+    $addrs = $_.GetIPProperties().UnicastAddresses
+    if ($addrs) {
+        foreach ($addr in $addrs) {
+            if ($addr.Address.AddressFamily -eq 'InterNetwork') {
+                if ($addr.Address.Address -ge $ipLow.Address -and $addr.Address.Address -le $ipHigh.Address) {
+                    $addr.Address
+                }
+            }
+        }
+    }
+}
